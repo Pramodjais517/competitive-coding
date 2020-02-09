cf38c61dd1b2bfa1ddc50cbe8ab5a92827ded353
Stark-Pramod <pramodjaiswal517@gmail.com>
2020-02-09 22:59:58 +0530

    cdf

---
+++ eucildean.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+// template starts here
+#define ll long long
+#define ull unsigned long long
+#define rs reserve
+#define pb push_back
+#define mp make_pair
+#define fi(i,s,e,inc) for(auto i=s;i<e;i+=inc)
+#define fie(i,s,e,inc) for(auto i=s;i<=e;i+=inc)
+#define fd(i,s,e,dec) for(auto i=s;i>e;i-=dec)
+#define fde(i,s,e,dec) for(auto i=s;i>=e;i-=dec)
+#define itr(i,ar) for(auto i=ar.begin();i!=ar.end();i++)
+#define show(ar) for(auto &i:ar) cout<<i<<" "
+#define mod 1000000007
+
+ll exp(ll a, ll b)
+{
+	if(b==0)
+	return 1;
+	if(b==1)
+	return a;
+	ll r = pow(a,b/2);
+	if(b&1)
+	return r*a*r;
+	return r*r;
+}
+ll gcd(int a,int b)
+{
+	if( b==0)
+	return a;
+	return gcd(b,a%b);
+}
+// template ends here
+
+
+int main()
+{
+//ll t;
+//cin>>t;
+//while(t--)
+//{
+//}
+ll n,k;
+cin>>n>>k;
+ll a[n];
+fi(i,0,n,1)
+cin>>a[i];
+ll g=a[0];
+for(ll i=0;i<n;i++)
+g = __gcd(g,a[i]);
+set<ll> s;
+for(ll i=0;i<k;i++)
+{
+	s.insert((g*i)%k);
+}
+cout<<s.size()<<"\n";
+show(s);
+cout<<"\n";
+return 0;
+}
+
