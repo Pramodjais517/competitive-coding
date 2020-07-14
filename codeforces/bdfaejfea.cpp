97d7baf08ea1682a7d19039c2df5258468e8c9a4
Stark-Pramod <pramodjaiswal517@gmail.com>
2020-07-14 05:48:03 +0530

    codeforces

---
+++ codeforces/bdfaejfea.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+// template starts here
+#define ll long long
+#define ull unsigned long long
+#define rs reserve
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define fi(i,s,e,inc) for(auto i=s;i<e;i+=inc)
+#define fie(i,s,e,inc) for(long long int i=s;i<=e;i+=inc)
+#define fd(i,s,e,dec) for(auto i=s;i>e;i-=dec)
+#define fde(i,s,e,dec) for(auto i=s;i>=e;i-=dec)
+#define itr(i,ar) for(auto i=ar.begin();i!=ar.end();i++)
+#define mod 1000000007
+
+ll N = 1000000;
+vector<bool> prime(N+1,true);
+
+void sieve()
+{
+	prime[0] = false,prime[1] = false;
+	for(ll i=2;i*i <= N;i++)
+	{
+		if(prime[i])
+		{
+			for(ll j = i*i; j<= N ;j+=i)
+			prime[j] = false;
+		}
+	}
+}
+
+ll pow(ll a, ll b)
+{
+	if(b==0)
+	return 1;
+	if(b==1)
+	return a;
+	ll r = pow(a,b/2);
+	if(b&1)
+	return r*a*r;
+	return r*r;
+}
+// template ends here
+
+
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+ll t;
+cin>>t;
+while(t--)
+{
+	ll n;
+	cin>>n;
+	ll ans = 0;
+	pair<ll,ll> p;
+	bool f = 0;
+	ll m = 1;
+	fie(i,3,sqrt(n),2)
+	{
+		if(n%i==0)
+		{
+			f=1;
+			m = i;
+			break;
+		}		
+	}
+	if(!f)
+	{
+		cout<<1<<" "<<n-1<<"\n";
+	}
+	else if(n%2 == 0)
+	{
+		cout<<n/2<<" "<<n/2<<"\n";
+	}
+	else
+	{
+		cout<<n/m<<" "<< (m-1)* (n / m)<<"\n";
+	}
+}
+return 0;
+}
+
